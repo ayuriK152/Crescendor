edce255cbea98b5e4c9e3dddb05ebdbf34917735
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2024-01-07 23:51:59 +0900

    feat: 미디 노트 생성 충돌현상 수정

--- PC/Assets/MidiTest.cs
+++ PC/Assets/MidiTest.cs
@@ -16,6 +16,7 @@ public class MidiTest : MonoBehaviour
     public TextMeshProUGUI noteText;
 
     public float noteScale = 1.0f;
+    public float scrollSpeed = 1.0f;
 
     List<GameObject> instatiateNotes = new List<GameObject>();
     List<Notes> notes = new List<Notes>();
@@ -23,7 +24,7 @@ public class MidiTest : MonoBehaviour
 
     void Awake()
     {
-        sourceFile = Resources.Load<TextAsset>("Converts/b9IsAwesome");
+        sourceFile = Resources.Load<TextAsset>("Converts/Nw_j_variations");
     }
 
     IEnumerator Start()
@@ -33,19 +34,26 @@ public class MidiTest : MonoBehaviour
         for (int i = 0; i < song.tracks.Count; i++)
         {
             MidiTrack track = song.tracks[i];
-            int startTime = -1;
+            int eventStartTime = -1;
             int deltaTime = 0;
             for (int j = 0; j < track.sequence.Count; j++)
             {
                 deltaTime = track.sequence[j].delta != 0 ? track.sequence[j].delta : deltaTime;
-                startTime += startTime == -1 ? 1 : track.sequence[j].delta;
-                if (track.sequence[j].midiEvent.status == 144)
+                eventStartTime += eventStartTime == -1 ? 1 : track.sequence[j].delta;
+                if (track.sequence[j].midiEvent.status == 144 && track.sequence[j].midiEvent.data2 > 0)
                 {
-                    tempNoteData.Add(track.sequence[j].midiEvent.data1, startTime);
+                    if (tempNoteData.ContainsKey(track.sequence[j].midiEvent.data1))
+                    {
+                        notes.Add(new Notes(track.sequence[j].midiEvent.data1, tempNoteData[track.sequence[j].midiEvent.data1], eventStartTime));
+                        tempNoteData.Remove(track.sequence[j].midiEvent.data1);
+                    }
+                    tempNoteData.Add(track.sequence[j].midiEvent.data1, eventStartTime);
                 }
-                else if (track.sequence[j].midiEvent.status == 128)
+                else if (track.sequence[j].midiEvent.status == 128 || (track.sequence[j].midiEvent.status == 144 && track.sequence[j].midiEvent.data2 == 0))
                 {
-                    notes.Add(new Notes(track.sequence[j].midiEvent.data1, tempNoteData[track.sequence[j].midiEvent.data1], startTime));
+                    if (!tempNoteData.ContainsKey(track.sequence[j].midiEvent.data1))
+                        continue;
+                    notes.Add(new Notes(track.sequence[j].midiEvent.data1, tempNoteData[track.sequence[j].midiEvent.data1], eventStartTime));
                     tempNoteData.Remove(track.sequence[j].midiEvent.data1);
                 }
             }
@@ -53,8 +61,8 @@ public class MidiTest : MonoBehaviour
         for (int i = 0; i < notes.Count; i++)
         {
             instatiateNotes.Add(Instantiate(noteObj, noteInstantiatePoint));
-            instatiateNotes[i].transform.localScale = new Vector3(0.5f, 0.5f, notes[i].deltaTime / 480.0f * noteScale);
-            instatiateNotes[i].transform.localPosition = new Vector3(notes[i].keyNum, 0, (notes[i].startTime / 480.0f + notes[i].deltaTime / 960.0f) * noteScale);
+            instatiateNotes[i].transform.localScale = new Vector3(0.5f, 0.5f, notes[i].deltaTime / (float)song.division * noteScale);
+            instatiateNotes[i].transform.localPosition = new Vector3(notes[i].keyNum, 0, (notes[i].startTime + notes[i].deltaTime / 2.0f) / song.division * noteScale);
         }
     }
 }
