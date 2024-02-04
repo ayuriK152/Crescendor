262dabb03acfaa9565495f2a8c33406feca01e7f
golapaduck <golapaduck29@naver.com>
2024-02-05 03:54:56 +0900

    feat: setscore, addscore API 추가

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,8 @@ const app = express()
 // configuration =========================
 app.set('port', process.env.PORT || 3000)
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Root')
 })
@@ -27,18 +29,45 @@ app.get('/record', (req, res) => {
     res.send(rows)
   })
 })
-
+// getscore API
 app.get('/record/getscore/:user_id/:music_id', (req, res) => {
   const user_id = req.params.user_id
   const music_id = Number(req.params.music_id)
 
   connection.query("SELECT score from Crescendor.record where (user_id = ? && music_id = ?);", [user_id, music_id], (error, rows) => {
     if (error) throw error
-    console.log('getscore \n user: %s \n music: %d', user_id, music_id)
+    console.log('getscore \n user: %s \n music: %d \n', user_id, music_id)
+    console.log(rows)
     res.send(rows)
   })
-  
 }) 
+// addscore API
+app.post('/record/addscore/:user_id/:music_id/', (req, res) => {
+  const user_id = req.params.user_id
+  const music_id = Number(req.params.music_id)
+  const { score, date, midi } = req.body
+
+  connection.query("INSERT INTO Crescendor.record SET user_id = ?, music_id = ? score = ?, date = ?, midi = ?;", [user_id, music_id, score, date, midi], (error, rows) => {
+    if (error) throw error
+    console.log('addscore \n user: %s \n music: %d \n', user_id, music_id)
+    res.send(rows)
+  })
+}) 
+// setscore API
+app.put('/record/setscore/:user_id/:music_id/', (req, res) => {
+  const user_id = req.params.user_id
+  const music_id = Number(req.params.music_id)
+  const { score, date, midi } = req.body
+
+  connection.query("UPDATE Crescendor.record SET score = ?, date = ?, midi = ? where (user_id = ? && music_id = ?);", [score, date, midi,user_id, music_id], (error, rows) => {
+    if (error) throw error
+    console.log('setscore \n user: %s \n music: %d \n', user_id, music_id)
+    console.log(rows)
+    res.send(rows)
+  })
+}) 
+
+
 app.get('/practice', (req, res) => {
   connection.query('SELECT * from Crescendor.practice;', (error, rows) => {
     if (error) throw error
