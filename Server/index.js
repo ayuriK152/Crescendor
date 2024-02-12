a5a6549362dc4ccb644ebc3c0c8a1847ceabeb93
golapaduck <golapaduck29@naver.com>
2024-02-12 20:09:32 +0900

    test: 블럭 처리

--- Server/index.js
+++ Server/index.js
@@ -72,7 +72,7 @@ app.post('/login', (req, res) => {
       res.status(400).send(`ERROR: id`)
       return
     }
-    user_password = rows[0].password
+    const { user_password } = rows[0]
     console.log(user_password)
     matchPassword = bcrypt.compare(password, user_password)
     
