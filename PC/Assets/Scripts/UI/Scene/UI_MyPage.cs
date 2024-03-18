7a56494d7d880d8f4931267733c415dfd91b880d
jhwong1234@gmail.com <jhwong1234@tukorea.ac.kr>
2024-03-18 23:46:00 +0900

    refactor: 마이페이지 버튼 수정

--- PC/Assets/Scripts/UI/Scene/UI_MyPage.cs
+++ PC/Assets/Scripts/UI/Scene/UI_MyPage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Windows;
 
@@ -11,7 +12,8 @@ public class UI_MyPage : UI_Scene
     TextMeshProUGUI nickname;
     enum Buttons
     {
-        BackButton,
+        MainMenuBtn,
+        SongSelectBtn,
     }
     // Start is called before the first frame update
     void Start()
@@ -21,13 +23,19 @@ public class UI_MyPage : UI_Scene
     public override void Init()
     {
         Bind<Button>(typeof(Buttons));
-        GetButton((int)Buttons.BackButton).gameObject.BindEvent(OnBacckBtnClick);
+        GetButton((int)Buttons.MainMenuBtn).gameObject.BindEvent(OnMainMenuBtnClick);
+        GetButton((int)Buttons.SongSelectBtn).gameObject.BindEvent(OnSongSelectBtnClick);
         string savedID = PlayerPrefs.GetString("PlayerID");
         nickname = GameObject.Find("UI_MyPage/UserInfo/Profile/Nickname").GetComponent<TextMeshProUGUI>();
         nickname.text = savedID;
     }
 
-    public void OnBacckBtnClick(PointerEventData data)
+    public void OnMainMenuBtnClick(PointerEventData data)
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void OnSongSelectBtnClick(PointerEventData data)
     {
         Managers.Scene.LoadScene(Define.Scene.SongSelectScene);
     }
